b0c5e7a6bdaafcc42a4d032735c31f58812d59ac
Rohit K <192195109+rohitkay@users.noreply.github.com>
2025-09-21 20:37:58 +1000

    feat: add user input for prefix

--- renamer.py
+++ renamer.py
@@ -6,3 +6,6 @@ def batch_rename():
     #set current directory path
     #get inputs from user for folder directory
     directory = input("Please provide the path to the directory you would like to batch rename within")
+
+    #get input for prefix to use
+    prefix = input("Plese type in the prefix to add to all files")
