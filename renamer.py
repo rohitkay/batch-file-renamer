b5deb12361481ff8092fbf345388c86ae3496875
Rohit K <192195109+rohitkay@users.noreply.github.com>
2025-09-21 20:03:01 +1000

    feat: add user input for directory path

--- renamer.py
+++ renamer.py
@@ -4,4 +4,5 @@ import os
 #Function to change the directory
 def batch_rename():
     #set current directory path
-    directory = ""
+    #get inputs from user for folder directory
+    directory = input("Please provide the path to the directory you would like to batch rename within")
